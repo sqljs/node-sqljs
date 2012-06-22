d8d8550717b1a7cea16cdee64a7a88f4b8f1a3f8
Pavel Lang <langpavel@phpskelet.org>
2012-06-22 02:36:05 +0200

    grammar and tests

--- tests/parser/09-02-00-identifiers.js
+++ tests/parser/09-02-00-identifiers.js
@@ -3,8 +3,8 @@ var rule_yields = require('./common/helper-rule-yields');
 
 
 
-exports['Grammar: QUOTED_ID "Identifier": valid input'] = rule_yields.bind(null, 
-  'QUOTED_ID', [
+exports['Grammar: ID "Identifier": valid input'] = rule_yields.bind(null, 
+  'ID', [
     'a', 'aa', 'aaa',
     'a1', '_1',
     '_$aaa', '$_aaa',
